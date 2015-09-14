aa3fa987669b1020d786d59f2c5a622e6ebe2257
barbatus <alex.borodach@gmail.com>
2015-09-14 21:44:25 +0300

    Adds logging of TS transpiler's diagnostics to the terminal.

--- plugin/ts_handler.js
+++ plugin/ts_handler.js
@@ -1,15 +1,46 @@
-var typescript = Npm.require('typescript');
+var ts = Npm.require('typescript');
+
+function logErrors(inputPath, compilerOptions) {
+  var program = ts.createProgram([inputPath], compilerOptions);
+
+  var syntacticDiagnostics = program.getSyntacticDiagnostics(null);
+  var semanticDiagnostics = program.getSemanticDiagnostics(null);
+  var diagnostics = syntacticDiagnostics.concat(semanticDiagnostics);
+  for (var i = 0; i < diagnostics.length; i++) {
+    var diagnostic = diagnostics[i];
+    if (diagnostic.file) {
+      var pos = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
+      var errorMessage = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+      var line = pos.line + 1;
+      var character = pos.character + 1;
+      var message = diagnostic.file.fileName + ' (' + line + ', ' + character + '): ' +
+        errorMessage;
+      console.warn(message);
+    }
+  }
+}
 
 Plugin.registerSourceHandler('ts', function(compileStep) {
-  var output = typescript.transpile(compileStep.read().toString('utf8'), {
-    module : typescript.ModuleKind.System
-  });
-  var moduleName = compileStep.inputPath.replace(/\\/,'/').replace('.ts','');
-  output = output.replace('System.register([', 'System.register("' + moduleName + '",[');
+  var source = compileStep.read().toString('utf8');
+  var outputFile = compileStep.inputPath;
+  var inputFile = compileStep.inputPath;
+
+  var path = inputFile.split('.ts');
+  var moduleId = path[0];
+
+  var options = {
+    module : ts.ModuleKind.System,
+    target: ts.ScriptTarget.ES5,
+    experimentalDecorators: true,
+    diagnostics: true
+  };
+  var result = ts.transpile(source, options, null, null, moduleId);
+
+  logErrors(inputFile, options);
 
   compileStep.addJavaScript({
-    path : compileStep.inputPath.replace('.ts', '.js'),
-    data : output,
-    sourcePath: compileStep.inputPath
+    path : outputFile,
+    data : result,
+    sourcePath: outputFile
   });
 });
